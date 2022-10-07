26faea10fc200141d0b39b7e647995b63161670a
AmandasFarias <61165518+AmandaFar@users.noreply.github.com>
2022-10-07 09:45:59 -0300

    Add files via upload

---
+++ infor.js
@@ -0,0 +1,68 @@
+//Espaço para os comentarios desse projeto back and
+
+/* Criação do servidor web c/ Node JS 
+-> modulo nativo: HTTP
+
+Servidor: nodemon index -> deixa o servidor rodando automaticamente
+e atualizada tudo  que há modificação
+
+Para instalar:
+cmd: npm install nodemon -g
+***esse -g significa global (modulo global)
+
+*testar e subir a aplicação:
+cmd: nodemon index
+
+**NPM -> gerenciador de pacotes do node js
+**package json -> onde fica toda a configuração, modulos
+
+***para estartar o package json:
+cmd: npm.init
+configura e ciar o pacote json
+
+***usando o modulo EXPRESS como dependencia do package json:
+cmd: npm install express --save
+
+-> esse -- save é para adicionar a informação  no package json
+
+***usando o modulo CONSIGN (carregamento de rotas)
+cmd: npm install consign --save
+
+***usando o modulo body-parser (p/ o metodo post, p/ interpretar os dados do post)
+cmd: npm install body-parser --save
+
+****usando o POSTMAN*****
+-> simula a rota e o método que vc quer é instalado.
+-> Acessar o site: get.postman.com
+
+***BANCO DE DADOS***
+-> Persistência de c/ NeDB (banco de dados do javascript)
+-> é preciso instalar e colocar como dependência
+cmd: npm install nedb --save
+
+**módulo de VALIDAÇÂO**
+cmd: npm install express-validator --save
+-> depois importa para o código index principal js
+
+*Método find -> traz varios registros
+
+*Método findOne -> traz apenas um registro
+
+***Métodos*****
+POST -> para cadastrar os dados
+
+PUT -> Alterar dados
+
+GET -> Acessar, consultar dados
+
+DELETE -> Excluir dados
+
+***já criados**** usar:
+
+UPDATE -> editar
+
+INSERT -> inserir
+
+REMOVER -> remover
+
+*/
